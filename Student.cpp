3fce1ff71f3ff802cd05afe482fcf15a82a6b1db
Zixuan Mao <zixuanmao@ucsb.edu>
2020-10-20 08:53:37 -0700

    Final

--- Student.cpp
+++ Student.cpp
@@ -1,33 +1,40 @@
 #include "Student.h"
 #include <sstream>
 
-Student::Student(int perm, 
-		 std::string lastName, 
-		 std::string firstAndMiddleNames) {
-  // THIS EMPTY CONSTRUCTOR IS A STUB
-  // DELETE THESE COMMENTS AND REPLACE WITH APPROPRIATE CODE
+Student::Student(int Perm, 
+		 std::string LastName, 
+		 std::string FirstAndMiddleNames) {
+       perm  = Perm;
+       lastName = LastName;
+       firstAndMiddleNames = FirstAndMiddleNames;
   // REMEMBER: PURPOSE OF A CONSTRUCTOR IS TO INITIALIZE 
   // ALL DATA MEMBERS WITH REASONABLE VALUES  
 }
 
 int Student::getPerm() const { 
-  return -999;
+  return perm;
 }
 
 std::string Student::getLastName() const { 
-  return "stub";
+  return lastName;
 }
 
 std::string Student::getFirstAndMiddleNames() const { 
-  return "stub";
+  return firstAndMiddleNames;
 }
 
 std::string Student::getFullName() const { 
-  return "stub";
+  return firstAndMiddleNames + " " + lastName;
 }
 
 std::string Student::toString() const { 
-  return "stub";
+  std::ostringstream oss;
+  
+  oss << "[" 
+      << this->getPerm() << ","
+      << this->getLastName() << ","
+      << this->getFirstAndMiddleNames() << "]";
+  return oss.str();
   /*
   // e.g. [12345,Smith,Malory Logan]
   std::ostringstream oss;
